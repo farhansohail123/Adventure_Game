68caf154f21ee0c21c623d4c1f17da8658f95375
farhansohail123 <muhammad.farhan19dec@gmail.com>
2023-11-08 00:19:46 +0500

    Initial commit

---
+++ main.ts
@@ -0,0 +1,56 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+class Player{
+    name:string;
+    fuel:number = 100;
+    constructor(name:string){
+        this.name = name;
+    }
+}
+
+class Opponent {
+    name:string;
+    fuel:number = 100;
+    constructor(name:string){
+        this.name = name;
+    }
+}
+
+let player = await inquirer.prompt({
+    type: "input",
+    name:"name",
+    message:"Please Enter Your Name :"
+})
+
+
+ let opponent = await inquirer.prompt({
+    type: "list",
+    name:"select",
+    message:"Select Your Opponent :",
+    choices:["Skeleton","Assasin","Zombie"]
+})
+console.log(opponent.select)
+
+let p1 = new Player(player.name)
+let o1 = new Opponent(player.name)
+
+if(opponent.select == "Skeleton"){
+    console.log(`${chalk.bold.green(p1.name)} VS ${chalk.bold.red(o1.name)}`)
+    let ask = await inquirer.prompt({
+        type: "list",
+        name:"opt",
+        message:"Select Your Opponent :",
+        choices:["Attack","Drink Portion","Run For Your Life...."],
+    });
+    if(ask.opt == "Attack"){
+        console.log("attack")
+    }
+    if(ask.opt == "Drink Portion"){
+        console.log("Drink")
+    }
+    if(ask.opt == "Run For Your Life..."){
+        console.log("run")
+    }
+}
+
